2da5f27bbbfcd569e5f2f4ca656109da2a32da5b
Jim van Kleef <jim.van.kleef@freiheit.com>
2018-12-19 14:45:20 +0100

    don't call onRecognitionComplete onEndOfSpeech for android, which causes it to be called twice with possibly different values

--- android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
+++ android/src/main/java/bz/rxla/flutter/speechrecognition/SpeechRecognitionPlugin.java
@@ -113,7 +113,9 @@ public class SpeechRecognitionPlugin implements MethodCallHandler, RecognitionLi
     @Override
     public void onEndOfSpeech() {
         Log.d(LOG_TAG, "onEndOfSpeech");
-        speechChannel.invokeMethod("speech.onRecognitionComplete", transcription);
+        // don't call this here because then onRecognitionComplete will be called twice and possibly
+        // with different values
+        //speechChannel.invokeMethod("speech.onRecognitionComplete", transcription);
     }
 
     @Override
